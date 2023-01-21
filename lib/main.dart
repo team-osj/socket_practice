af93dde56d8868b54a639eb122be830e2b2e2c75
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-22 01:26:48 +0900

    stream builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_practice/baseurl.dart';
+import 'dart:async';
 
 void main() => runApp(const MyApp());
 
@@ -23,6 +24,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  StreamSocket streamSocket = StreamSocket();
+
   IO.Socket socket = IO.io(
       '$url/application',
       IO.OptionBuilder()
@@ -32,7 +35,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _connect() {
     socket.onConnect((data) => debugPrint('연결 성공'));
-    socket.on('update', (data) => debugPrint('받은 데이터 : $data'));
+    socket.on('update', (data) {
+      debugPrint('받은 값 : $data');
+      return streamSocket.addResponse;
+    });
     socket.onDisconnect((data) => debugPrint('끊김'));
     socket.connect();
   }
@@ -45,6 +51,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: streamSocket.getResponse,
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Center(child: Text(snapshot.data.toString()));
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("에러"),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class StreamSocket {
+  final _socketResponse = StreamController<String>();
+
+  void Function(String) get addResponse => _socketResponse.sink.add;
+
+  Stream<String> get getResponse => _socketResponse.stream;
+
+  void dispose() {
+    _socketResponse.close();
   }
 }
